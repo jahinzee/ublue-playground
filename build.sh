b85e191007c97d5ff4c2c1840c135620b857a95f
jahinzee <jahinzee@outlook.com>
2024-06-20 17:45:03 +1000

    refactor: multi-script to single-script

--- build.sh
+++ build.sh
@@ -25,4 +25,16 @@ systemctl enable podman.socket
 
 rpm-ostree install fastfetch
 
-source ./brew.sh
+#### Linuxbrew
+
+# Instructions based off brew.sh from ublue-os/bluefin
+# https://github.com/ublue-os/bluefin/blob/main/build_files/base/brew.sh (author: m2Giles)
+
+touch /.dockerenv
+mkdir -p /var/home
+mkdir -p /var/roothome
+
+curl -Lo /tmp/brew-install https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+chmod +x /tmp/brew-install
+/tmp/brew-install
+tar --zstd -cvf /usr/share/homebrew.tar.zst /home/linuxbrew/.linuxbrew
