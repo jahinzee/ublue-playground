0484ca22520b60d943b4f7a4fe0af4b67ed0317a
jahinzee <jahinzee@outlook.com>
2024-06-30 20:16:17 +1000

    reshuffling + exit logging - looks like brew is interrupting something, so let's try reshuffling stuff

--- build.sh
+++ build.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 set -ouex pipefail
+trap 'echo :::: Now exiting...' EXIT
 
 RELEASE="$(rpm -E %fedora)"
 
 # Most of these commands are based on the build
 # scripts in https://github.com/ublue-os/bluefin.
+
+# ---
+echo ":: BRANDING"
+# ---
+
+sed -i '/^PRETTY_NAME/s/Kinoite/bp_OS/' /usr/lib/os-release
+sed -i "/^PRETTY_NAME/s/Fedora Linux/bp_OS/" /usr/lib/os-release
+sed -i '/^NAME/s/Fedora Linux/bp_OS/' /usr/lib/os-release
+sed -i '/^DEFAULT_HOSTNAME/s/fedora/bpos/' /usr/lib/os-release
+sed -i '/^HOME_URL/s/https:\/\/kinoite.fedoraproject.org/https:\/\/github.com\/jahinzee\/ublue-playground/' /usr/lib/os-release
+sed -i '/^DOCUMENTATION_URL/s/https:\/\/kinoite.fedoraproject.org/https:\/\/github.com\/jahinzee\/ublue-playground/' /usr/lib/os-release
+sed -i '/^SUPPORT_URL/s/https:\/\/kinoite.fedoraproject.org/https:\/\/github.com\/jahinzee\/ublue-playground/' /usr/lib/os-release
+sed -i '/^BUG_REPORT_URL/s/https:\/\/kinoite.fedoraproject.org/https:\/\/github.com\/jahinzee\/ublue-playground/' /usr/lib/os-release
+
+cp /tmp/watermark.png /usr/share/plymouth/themes/spinner/watermark.png
 
 # ---
 echo ":: PACKAGES"
@@ -22,20 +38,8 @@ mkdir -p /var/roothome
 
 curl -Lo /tmp/brew-install https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
 chmod +x /tmp/brew-install
-/tmp/brew-install
+/tmp/brew-install || true
 tar --zstd -cvf /usr/share/homebrew.tar.zst /home/linuxbrew/.linuxbrew
 
 # ---
-echo ":: BRANDING"
-# ---
-
-sed -i '/^PRETTY_NAME/s/Kinoite/bp_OS/' /usr/lib/os-release
-sed -i "/^PRETTY_NAME/s/Fedora Linux/bp_OS/" /usr/lib/os-release
-sed -i '/^NAME/s/Fedora Linux/bp_OS/' /usr/lib/os-release
-sed -i '/^DEFAULT_HOSTNAME/s/fedora/bpos/' /usr/lib/os-release
-sed -i '/^HOME_URL/s/https:\/\/kinoite.fedoraproject.org/https:\/\/github.com\/jahinzee\/ublue-playground/' /usr/lib/os-release
-sed -i '/^DOCUMENTATION_URL/s/https:\/\/kinoite.fedoraproject.org/https:\/\/github.com\/jahinzee\/ublue-playground/' /usr/lib/os-release
-sed -i '/^SUPPORT_URL/s/https:\/\/kinoite.fedoraproject.org/https:\/\/github.com\/jahinzee\/ublue-playground/' /usr/lib/os-release
-sed -i '/^BUG_REPORT_URL/s/https:\/\/kinoite.fedoraproject.org/https:\/\/github.com\/jahinzee\/ublue-playground/' /usr/lib/os-release
-
-cp /tmp/watermark.png /usr/share/plymouth/themes/spinner/watermark.png
+echo ":: DONE"
