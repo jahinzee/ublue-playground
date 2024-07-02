8acf8cac85ecb8e1028a0770c32c364f4dbd9ac3
jahinzee <jahinzee@outlook.com>
2024-07-02 13:07:48 +1000

    broadcom + virtualisation + new watermark

--- build.sh
+++ build.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 set -ouex pipefail
-trap 'echo :::: Now exiting...' EXIT
+trap 'echo -e :: Exiting build script.\\n:: If you see this message without a \"Build completed.\" message, an error may have occured.' EXIT
 
 RELEASE="$(rpm -E %fedora)"
 
-# Most of these commands are based on the build
+# Some commands are based on the build
 # scripts in https://github.com/ublue-os/bluefin.
 
+echo '    _              ___  ____     '
+echo '   | |__  _ __    / _ \/ ___|    '
+echo '   | '_ \| '_ \  | | | \___ \    '
+echo '   | |_) | |_) | | |_| |___) |   '
+echo '   |_.__/| .__/___\___/|____/    '
+echo '         |_| |_____|             '
+echo '                                 '
+echo " ~ jahinzee's uBlue Playground ~ "
+echo ""
+
 # ---
-echo ":: BRANDING"
+echo ":: Adding branding"
 # ---
 
-sed -i '/^PRETTY_NAME/s/Kinoite/bp_OS/' /usr/lib/os-release
+sed -i "/^PRETTY_NAME/s/Kinoite/Testing build – not for general consumption!/" /usr/lib/os-release
 sed -i "/^PRETTY_NAME/s/Fedora Linux/bp_OS/" /usr/lib/os-release
 sed -i '/^NAME/s/Fedora Linux/bp_OS/' /usr/lib/os-release
 sed -i '/^DEFAULT_HOSTNAME/s/fedora/bpos/' /usr/lib/os-release
@@ -23,13 +33,36 @@ sed -i '/^BUG_REPORT_URL/s/https:\/\/kinoite.fedoraproject.org/https:\/\/github.
 cp /tmp/watermark.png /usr/share/plymouth/themes/spinner/watermark.png
 
 # ---
-echo ":: PACKAGES"
+echo ":: Installing packages: Distrobox"
 # ---
 
-rpm-ostree install podman distrobox
+rpm-ostree install \
+    podman \
+    distrobox
 
 # ---
-echo ":: LINUXBREW"
+echo ":: Installing packages: Drivers/Broadcom"
+# ---
+
+# TODO: add a Broadcom target build
+rpm-ostree install \
+    broadcom-wl
+
+# ---
+echo ":: Installing packages: Virtualisation"
+# ---
+
+rpm-ostree install \
+    virt-install \
+    libvirt-daemon-config-network \
+    libvirt-daemon-kvm \
+    qemu-kvm \
+    virt-manager \
+    virt-viewer
+systemctl enable libvirtd
+
+# ---
+echo ":: Installing Linuxbrew"
 # ---
 
 touch /.dockerenv
@@ -42,4 +75,5 @@ chmod +x /tmp/brew-install
 tar --zstd -cvf /usr/share/homebrew.tar.zst /home/linuxbrew/.linuxbrew
 
 # ---
-echo ":: DONE"
+echo ":: Build complete."
+# ---
